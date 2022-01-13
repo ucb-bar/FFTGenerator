272292736b014cdbac2fb617667121b21c5bc5b0
Animesh Agrawal <animesha@berkeley.edu>
2022-01-13 10:07:34 -0800

    Commented alternate way of generating test points

--- test_pts.py
+++ test_pts.py
@@ -14,20 +14,27 @@ points = []
 for n in points_n:
     exponent = (-2 * pi * (freq / freq_samp) * complex(0, n) )
     points.append(e ** exponent) # complex(0, n) = nj
-    point = points[-1]
+    # numerator = (e ** complex(0, n)) - (e ** complex(0, -n))
+    # denominator = complex(0, 2)
+    # sin_n = numerator/denominator
+    # points.append(sin_n)
 
 # pip install numfi
 from numfi import *
 
+points_in_bin = ""
 for n in points_n:
     point = points[n - 1]
     numfi_real = numfi(point.real, 1, IOWidth, bp) # arg0: input float, arg1: signed(1)/unsigned(0), arg2: total width, arg3: fracwidth
     numfi_imag = numfi(point.imag, 1, IOWidth, bp)
     print(f"=== POINT {n} === ")
-    # print(f"Real:\n\tFloat:{point.real}\n\tFP:{numfi_real}\n\tBIN:{numfi_real.bin}")
-    # print(f"Imag:\n\tFloat:{point.imag}\n\tFP:{numfi_imag}\n\tBIN:{numfi_imag.bin}")
+    print(f"Real:\n\tFloat:{point.real}\n\tFP:{numfi_real}\n\tBIN:{numfi_real.bin}")
+    print(f"Imag:\n\tFloat:{point.imag}\n\tFP:{numfi_imag}\n\tBIN:{numfi_imag.bin}")
     real_bits = f"{numfi_real.bin}"[2:-2]
     imag_bits = f"{numfi_imag.bin}"[2:-2]
     print(f"fft-test: \n\tbinary input: {real_bits + imag_bits}")
     print()
+    points_in_bin += f"0b{(real_bits + imag_bits)},\n"
 
+print("For easy copy-paste:")
+print(points_in_bin.strip()[:-1])
